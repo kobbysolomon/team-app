2c82ab246b51f736877eab0979b5c68edce94202
Godfried Solomon <godfried.solomon@apollomce.com>
2023-07-30 23:08:01 -0400

    project completed

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
-export class TeamComponent {
+export class TeamComponent implements OnInit{
+
+  @Input() team: string[] = []
+  @Input() index = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
